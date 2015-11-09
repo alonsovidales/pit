ecd0ff2a52093fd9bd8bd45f224c79e3b39780a9
Alonso Vidales <alonso.vidales@tras2.es>
2015-11-09 22:51:05 +0100

    Code linting

--- billing/billing.go
+++ billing/billing.go
@@ -10,12 +10,14 @@ import (
 	"time"
 )
 
+// Billing Structure used to manage all the communication with the billing
+// provider, in this case PayPal
 type Billing struct {
 	token           string
 	tokenExpireTs   int64
 	url             string
-	baseUri         string
-	clientId        string
+	baseURI         string
+	clientID        string
 	secret          string
 	email           string
 	bussinessName   string
@@ -26,23 +28,27 @@ type Billing struct {
 	addrCountryCode string
 }
 
+// InvMerchantInfo Information of the client
 type InvMerchantInfo struct {
 	Email        string            `json:"email"`
 	BusinessName string            `json:"business_name"`
 	Address      map[string]string `json:"address"`
 }
 
+// InvItemPrize Price for the item to be billed
 type InvItemPrize struct {
 	Currency string `json:"currency"`
 	Value    int    `json:"value"`
 }
 
+// InvItem Item name and quantity
 type InvItem struct {
 	Name      string        `json:"name"`
 	Quantity  float64       `json:"quantity"`
 	UnitPrice *InvItemPrize `json:"unit_price"`
 }
 
+// Invoice Information for an invoice
 type Invoice struct {
 	MerchantInfo *InvMerchantInfo    `json:"merchant_info"`
 	BillingInfo  []map[string]string `json:"billing_info"`
@@ -50,10 +56,12 @@ type Invoice struct {
 	Note         string              `json:"note"`
 }
 
-func GetBilling(baseUri, clientId, secret, email, bussinessName, addrAddr, addrCity, addrState, addrZip, addrCountryCode string) (bi *Billing) {
+// GetBilling Returns an object that allows to manage all the information of
+// the billing provider
+func GetBilling(baseURI, clientID, secret, email, bussinessName, addrAddr, addrCity, addrState, addrZip, addrCountryCode string) (bi *Billing) {
 	bi = &Billing{
-		baseUri:         baseUri,
-		clientId:        clientId,
+		baseURI:         baseURI,
+		clientID:        clientID,
 		secret:          secret,
 		email:           email,
 		bussinessName:   bussinessName,
@@ -67,7 +75,8 @@ func GetBilling(baseUri, clientId, secret, email, bussinessName, addrAddr, addrC
 	return
 }
 
-func (bi *Billing) SendNewBill(invTitle, targetEmail string, items []*InvItem) (billId string, err error) {
+// SendNewBill Generates and sends a new bill to the provider to be processed
+func (bi *Billing) SendNewBill(invTitle, targetEmail string, items []*InvItem) (billID string, err error) {
 	inv := &Invoice{
 		MerchantInfo: &InvMerchantInfo{
 			Email:        bi.email,
@@ -91,29 +100,30 @@ func (bi *Billing) SendNewBill(invTitle, targetEmail string, items []*InvItem) (
 
 	invStr, _ := json.Marshal(inv)
 
-	if body, err := bi.callToApi("invoicing/invoices", "POST", string(invStr)); err == nil {
+	if body, err := bi.callToAPI("invoicing/invoices", "POST", string(invStr)); err == nil {
 		responseInfo := make(map[string]interface{})
 		err = json.Unmarshal(body, &responseInfo)
 		if err != nil {
 			log.Error("Problem trying to response from PayPal API, Error:", err)
 		}
 
-		billId = responseInfo["id"].(string)
-		sendRes, err := bi.callToApi(fmt.Sprintf("invoicing/invoices/%s/send", billId), "POST", "")
+		billID = responseInfo["id"].(string)
+		sendRes, err := bi.callToAPI(fmt.Sprintf("invoicing/invoices/%s/send", billID), "POST", "")
 
-		log.Info("Sent!!!:", fmt.Sprintf("invoicing/invoices/%s/send", billId), string(sendRes), err)
+		log.Info("Sent!!!:", fmt.Sprintf("invoicing/invoices/%s/send", billID), string(sendRes), err)
 	}
 
 	return
 }
 
+// getToken Retreves and returns the token from the provider
 func (bi *Billing) getToken() {
 	client := &http.Client{}
 
-	req, err := http.NewRequest("POST", bi.baseUri+"oauth2/token", strings.NewReader("grant_type=client_credentials"))
+	req, err := http.NewRequest("POST", bi.baseURI+"oauth2/token", strings.NewReader("grant_type=client_credentials"))
 	req.Header.Add("Accept", `application/json`)
 	req.Header.Add("Accept-Language", `en_US`)
-	req.SetBasicAuth(bi.clientId, bi.secret)
+	req.SetBasicAuth(bi.clientID, bi.secret)
 	resp, err := client.Do(req)
 	if err != nil {
 		log.Error("Problem trying to request a new token from the PayPal API, Error:", err)
@@ -134,14 +144,15 @@ func (bi *Billing) getToken() {
 	bi.tokenExpireTs = time.Now().Unix() + int64(responseInfo["expires_in"].(float64))
 }
 
-func (bi *Billing) callToApi(uri string, method string, reqBody string) (body []byte, err error) {
+// callToAPI Performs an HTTP request to the billing provider API
+func (bi *Billing) callToAPI(uri string, method string, reqBody string) (body []byte, err error) {
 	if time.Now().Unix() > bi.tokenExpireTs {
 		bi.getToken()
 	}
 
 	client := &http.Client{}
 
-	url := bi.baseUri + uri
+	url := bi.baseURI + uri
 	log.Info(reqBody)
 	req, err := http.NewRequest(method, url, strings.NewReader(reqBody))
 	req.Header.Add("Content-Type", `application/json`)
