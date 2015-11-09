22bddae1a43ac7fd4926e762070decee3d33690a
Alonso Vidales <alonso.vidales@tras2.es>
2015-11-09 22:22:59 +0100

    Code linting

--- accounts_manager/accounts_manager.go
+++ accounts_manager/accounts_manager.go
@@ -16,18 +16,31 @@ import (
 )
 
 const (
-	CBillingInfo     = "/billing_info"
-	CRegisterPath    = "/account_register"
-	CVerifyPath      = "/account_verify"
-	CLogsPath        = "/account_logs"
-	CDisablePath     = "/disable_account"
+	// CBillingInfo Endpoint for the service that will provide the client
+	// billing info
+	CBillingInfo = "/billing_info"
+	// CRegisterPath Endpoint to be used to register new system accounts
+	CRegisterPath = "/account_register"
+	// CVerifyPath Endpoint to be used to verify the identity of an account
+	CVerifyPath = "/account_verify"
+	// CLogsPath Endpoint that will return the logs for an account
+	CLogsPath = "/account_logs"
+	// CDisablePath Endpoint to be used to disable an account
+	CDisablePath = "/disable_account"
+	// CRecoverPassPath Endpoint to be used to recover for account password
+	// recovery
 	CRecoverPassPath = "/recover_pass"
-	CChangePass      = "/change_pass"
-	CResetPass       = "/reset_pass" // HTML content
+	// CChangePass Endpoint to be used to change an account password
+	CChangePass = "/change_pass"
+	// CResetPass Endpoint to be used to reset the password for an account
+	CResetPass = "/reset_pass"
 
+	// cConfirmEmailTTL Time in seconds that a recovery password token is
+	// going to be valid
 	cConfirmEmailTTL = 24 * 3600
 )
 
+// Manager Strcuture that will be used to manage all the accounts in the system
 type Manager struct {
 	usersModel     users.ModelInt
 	baseURL        string
@@ -37,6 +50,8 @@ type Manager struct {
 	mailServerPort int64
 }
 
+// Init Initialize a Manager struct with the corresponding parameters and
+// returns it
 func Init(baseURL, mailFromAddr, mailServerAddr string, mailServerPort int64, usersModel users.ModelInt) (mg *Manager) {
 	mg = &Manager{
 		baseURL:        baseURL,
@@ -50,6 +65,7 @@ func Init(baseURL, mailFromAddr, mailServerAddr string, mailServerPort int64, us
 	return
 }
 
+// BillingInfo Returns as JSON the billing info for the given account
 func (mg *Manager) BillingInfo(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 
@@ -73,6 +89,7 @@ func (mg *Manager) BillingInfo(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// Register a new account on the system
 func (mg *Manager) Register(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 
@@ -126,6 +143,8 @@ func (mg *Manager) Register(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Verification e-mail sent, please check your e-mail!"))
 }
 
+// Verify This method will be called in order to verify the identity of an
+// account, using the link from the e-mail provided during the registration
 func (mg *Manager) Verify(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 
@@ -161,6 +180,7 @@ func (mg *Manager) Verify(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// Logs Returns the logs for a provided account
 func (mg *Manager) Logs(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 
@@ -184,6 +204,7 @@ func (mg *Manager) Logs(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// Disable Disables an account disallowing any future access to it
 func (mg *Manager) Disable(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 
@@ -201,6 +222,7 @@ func (mg *Manager) Disable(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// ChangePass Verify and changes the password for a speciied account
 func (mg *Manager) ChangePass(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 
@@ -235,6 +257,9 @@ func (mg *Manager) ChangePass(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// RecoverPass This method generates a "recovery token" that is sent to the
+// account associated e-mail, from the link provided on the e-mail the user can
+// restore the account password
 func (mg *Manager) RecoverPass(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 
@@ -276,10 +301,14 @@ func (mg *Manager) RecoverPass(w http.ResponseWriter, r *http.Request) {
 	return
 }
 
+// getSignature Generates a signature from the provided parameters that can be
+// used to verify or sign this
 func (mg *Manager) getSignature(uid, keyHash string, ttl int64) string {
 	return fmt.Sprintf("%x", sha1.Sum([]byte(fmt.Sprintf("%s:%s:%d:%s", uid, keyHash, ttl, mg.secret))))
 }
 
+// SendEmail Sends an e-mail to the specified addess with the specified subject
+// and body
 func (mg *Manager) SendEmail(to, body, subject string) (success bool) {
 	auth := smtp.PlainAuth(
 		mg.mailFromAddr,
